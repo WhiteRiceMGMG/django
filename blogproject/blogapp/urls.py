19954a20448ee91c411ad2b1caa8a65748258423
ogawa <3223mq118@gmail.com>
2026-05-19 21:09:01 +0900

    template

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -4,5 +4,13 @@ from . import views
 app_name = 'blogapp'
 
 urlpatterns = [
-    path('', views.index_view, name='index')
+    path('', views.index_view, name='index'),
+    path(
+        #詳細ページのURLはblog-detail/レコードのid
+        'blog-detail/<int:pk>/',
+        #viewモジュールのblog detailを実行
+        views.blog_detail,
+        #URLパターンの名前をblog_detailにする
+        name='blog_detail'
+        ),
 ]
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,4 +1,39 @@
 from django.shortcuts import render
 
+#モデルからBlogPostをインポート
+from .models import BlogPost
+
+
 def index_view(request):
-    return render(request, 'index.html')
+    '''
+    トップページのビュー
+    テンプレートをレンダリングして戻り値として返す
+    Parameters:
+        request(HTTPRequest):
+            クライアントからのリクエスト情報を格納した
+            HTTPRequestオブジェクト
+    Return(HTTPRequest):
+        render()でテンプレートをレンダリングした結果
+    モデルBloPostのオ
+    ブジェクトにorder_by()を適用して，
+    BlogPostのレコードを投稿日時の降順で並び替える
+    '''
+
+    records = BlogPost.objects.order_by('-posted_at')
+    '''
+    render():
+    第一引数:HTTPRequestオブジェクト
+    第二引数:レンダリングするテンプレート
+  　第三引数:テンプレートに引き渡すdict型のデータ
+             {任意のキー:クエリの結果(レコードのリスト)}
+    '''
+
+    return render(request, 'index.html', {'orderby_records': records})
+
+def blog_detail(request, pk):
+    '''
+    pk:投稿記事のid
+    '''
+    record = BlogPost.objects.get(id=pk)
+    return render(
+        request, 'post.html', {'object': record})
